82e8664f0104097489cd63badc8aa5fb5948e16f
tuanw <tuanw@TUAN>
2022-10-23 13:01:05 +0700

    Add project files.

---
+++ DACN/Controllers/UserLoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DACN.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        // GET: UserLogin
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ DACN/Models/UserLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace DACN.Models
+{
+    public class UserLogin
+    {
+        [Key]
+        [Display(Name = "Tên đăng nhập")]
+        [Required(ErrorMessage = "Không được bỏ trống!")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage = "Không được bỏ trống")]
+        public string Password { get; set; }
+        public string GroupID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public int Status { get; set; }
+    }
+}
